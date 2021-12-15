ac6e5097ed2c3871be5fa141104823dffb1ce263
Herman Yeh <bcye2002@gmail.com>
2021-12-15 18:37:37 +0800

    Test_1

---
+++ test_1215.js
@@ -0,0 +1,22 @@
+let a = 123;
+let b = 456;
+console.log(a+b);
+let name2 = "Jeff";
+let message2 = "要下課了~";
+
+console.log("-".repeat(50));
+console.log(name2,message2);
+console.log("-".repeat(50));
+let d = true;
+let e = false;
+
+console.log(d && e);
+console.log(d || e);
+
+let f = a > 100;
+console.log("f:",f);
+console.log("-".repeat(30));
+let time = new Date();
+console.log(time.getFullYear());
+console.log(time.getMonth());
+console.log(time.getMonth()-1);
